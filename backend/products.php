597c721587d0ec495db58ccbd350f5015d30c9f7
ranajeevan-lab <rana.jeevan@gmail.com>
2026-08-05 23:02:14 +0530

    Fixid the connectivity issue

--- backend/products.php
+++ backend/products.php
@@ -1,6 +1,11 @@
 <?php
 require_once 'includes/config.php';
 require_once 'includes/db.php';
+
+$result = mysqli_query(
+    $conn,
+    'SELECT id, product_name, quantity, price FROM products ORDER BY id'
+);
 ?>
 
 <!DOCTYPE html>
@@ -12,8 +17,6 @@ require_once 'includes/db.php';
 
 <h1>Products</h1>
 
-<p>Product list will be displayed here.</p>
-
 <table border="1" cellpadding="5">
     <tr>
         <th>ID</th>
@@ -21,6 +24,15 @@ require_once 'includes/db.php';
         <th>Quantity</th>
         <th>Price</th>
     </tr>
+
+    <?php while ($product = mysqli_fetch_assoc($result)): ?>
+    <tr>
+        <td><?php echo $product['id']; ?></td>
+        <td><?php echo htmlspecialchars($product['product_name']); ?></td>
+        <td><?php echo $product['quantity']; ?></td>
+        <td><?php echo number_format((float) $product['price'], 2); ?></td>
+    </tr>
+    <?php endwhile; ?>
 </table>
 
 </body>
